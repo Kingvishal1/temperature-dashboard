6e06ff71f53cc4df489baa2896aa77921f153d7a
Kingvishal1 <vramaprabhu@gmail.com>
2025-10-15 00:31:28 +0530

    feat(print): add print statements

--- app.py
+++ app.py
@@ -3,15 +3,14 @@ from flask import Flask, request, jsonify, send_from_directory
 import os
 from src.api import temp
 
-
 SHARED_TOKEN = "G1egViAPwPWO6x3b9UZIjx5eZcFPmVdQtWS946TjaBxVaUxYQ1u7fhtcSZc1DKyb"
 
 app = Flask(__name__, static_folder="static", static_url_path="/static")
 
 
-
 @app.route("/api/temps", methods=["POST"])
 def receive_temp():
+    print("received request: " + request.get_json)
     return temp.receive_device_info(request.get_json(force=True))
 
 
